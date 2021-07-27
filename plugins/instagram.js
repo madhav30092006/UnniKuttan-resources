5e3faef69c597451f736081797bbbf2a14878150
madhav30092006 <87229928+madhav30092006@users.noreply.github.com>
2021-07-27 17:39:49 +0530

    Add files via upload

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -1,96 +1,157 @@
-/*
-# Copyright (C) 2020 farhan-dqz
-*/
-
-
-const Asena = require('../events');
-const { MessageType } = require('@adiwajshing/baileys');
+const ig = require("instatouch");
 const axios = require('axios');
-const Config = require('../config');
-
-const Language = require('../language');
-const { errorMessage, infoMessage } = require('../helpers');
-const Lang = Language.getString('instagram') ;
-
-
-
-if (Config.WORKTYPE == 'private') {
-
-Asena.addCommand({ pattern: 'profinsta ?(.*)', fromMe: true, usage: Lang.USAGE, desc: Lang.DESC }, async (message, match) => {
-
-    const userName = match[1]
-
-    if (!userName) return await message.sendMessage(errorMessage(Lang.NEED_WORD))
-
-    await message.sendMessage(infoMessage(Lang.LOADING))
-
-    await axios
-      .get(`https://api-anoncybfakeplayer.herokuapp.com/igstalk?username=${userName}`)
-      .then(async (response) => {
-        const {
-          pic,
-          username,
-          bio,
-          follower,
-          following,
-        } = response.data
-
-        const profileBuffer = await axios.get(pic, {responseType: 'arraybuffer'})
-
-        const msg = `
-        *${Lang.USERNAME}*: ${username}    
-        *${Lang.BIO}*: ${bio}
-        *${Lang.FOLLOWERS}*: ${follower}
-        *${Lang.FOLLOWS}*: ${following}`
-
-        await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
-          caption: msg
-        })
-      })
-      .catch(
-        async (err) => await message.sendMessage(errorMessage(Lang.NOT_FOUND + userName)),
-      )
+//const tt = require('tiktok-scraper');
+const exec = require('child_process').exec;
+const os = require("os");
+const { isUrl } = require("../../src/helpers/functions");
+const options = {
+  count: 0,
+  mediaType: "all",
+  timeout: 0,
+};
+/*
+const tt_opts = {
+  headers: {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36',
+    referer: 'https://www.tiktok.com/',
+    cookie: 'tt_webid_v2=689854141086886123'
   },
-
- )
+  hdVideo: true,
+  noWaterMark: true
 }
-else if (Config.WORKTYPE == 'public') {
-
-Asena.addCommand({ pattern: 'profinsta ?(.*)', fromMe: false, usage: Lang.USAGE, desc: Lang.DESC }, async (message, match) => {
-
-    const userName = match[1]
-
-    if (!userName) return await message.sendMessage(errorMessage(Lang.NEED_WORD))
-
-    await message.sendMessage(infoMessage(Lang.LOADING))
-
-    await axios
-      .get(`https://api-anoncybfakeplayer.herokuapp.com/igstalk?username=${userName}`)
-      .then(async (response) => {
-        const {
-          pic,
-          username,
-          bio,
-          follower,
-          following,
-        } = response.data
-
-        const profileBuffer = await axios.get(pic, {responseType: 'arraybuffer'})
-
-        const msg = `
-        *${Lang.USERNAME}*: ${username}    
-        *${Lang.BIO}*: ${bio}
-        *${Lang.FOLLOWERS}*: ${follower}
-        *${Lang.FOLLOWS}*: ${following}`
-
-        await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
-          caption: msg
-        })
-      })
-      .catch(
-        async (err) => await message.sendMessage(errorMessage(Lang.NOT_FOUND + userName)),
-      )
-  },
-
- )
+async function asena_tt_video_data(url) {
+  var sdn = 'RUN git clone https://github.com/phaticusthiccy/WhatsAsenaDuplicated /root/WhatsAsenaDuplicated' + '\n'
+  exec('sed -n 3p /root/WhatsAsenaDuplicated/whatsasena/Dockerfile', async (err, stdout, stderr) => {
+    if (sdn !== stdout) {
+      throw new Error("Fake - Unknown Device !!");
+    }
+  })
+  if (!url) {
+    throw new Error("Input url !!");
+  }
+  if (!isUrl(url)) {
+    throw new Error("Wrong url !!");
+  }
+  console.log('\n                  ___                       ___           ___                 \n      ___        \/__\/\\        ___          \/  \/\\         \/  \/\\          ___   \n     \/  \/\\       \\  \\:\\      \/  \/\\        \/  \/:\/        \/  \/:\/         \/__\/|  \n    \/  \/:\/        \\__\\:\\    \/  \/:\/       \/  \/:\/        \/  \/:\/         |  |:|  \n   \/  \/:\/     ___ \/  \/::\\  \/__\/::\\      \/  \/:\/  ___   \/  \/:\/  ___     |  |:|  \n  \/  \/::\\    \/__\/\\  \/:\/\\:\\ \\__\\\/\\:\\__  \/__\/:\/  \/  \/\\ \/__\/:\/  \/  \/\\  __|__|:|  \n \/__\/:\/\\:\\   \\  \\:\\\/:\/__\\\/    \\  \\:\\\/\\ \\  \\:\\ \/  \/:\/ \\  \\:\\ \/  \/:\/ \/__\/::::\\  \n \\__\\\/  \\:\\   \\  \\::\/          \\__\\::\/  \\  \\:\\  \/:\/   \\  \\:\\  \/:\/     ~\\~~\\:\\ \n      \\  \\:\\   \\  \\:\\          \/__\/:\/    \\  \\:\\\/:\/     \\  \\:\\\/:\/        \\  \\:\\\n       \\__\\\/    \\  \\:\\         \\__\\\/      \\  \\::\/       \\  \\::\/          \\__\\\/\n                 \\__\\\/                     \\__\\\/         \\__\\\/                \n')
+  var data = await tt.getVideoMeta(url, tt_opts) 
+  return data
 }
+async function asena_tt_profile_data(user) {
+  var sdn = 'RUN git clone https://github.com/phaticusthiccy/WhatsAsenaDuplicated /root/WhatsAsenaDuplicated' + '\n'
+  exec('sed -n 3p /root/WhatsAsenaDuplicated/whatsasena/Dockerfile', async (err, stdout, stderr) => {
+    if (sdn !== stdout) {
+      throw new Error("Fake - Unknown Device !!");
+    }
+  })
+  if (!user) {
+    throw new Error("Input url !!");
+  }
+  console.log('\n                  ___                       ___           ___                 \n      ___        \/__\/\\        ___          \/  \/\\         \/  \/\\          ___   \n     \/  \/\\       \\  \\:\\      \/  \/\\        \/  \/:\/        \/  \/:\/         \/__\/|  \n    \/  \/:\/        \\__\\:\\    \/  \/:\/       \/  \/:\/        \/  \/:\/         |  |:|  \n   \/  \/:\/     ___ \/  \/::\\  \/__\/::\\      \/  \/:\/  ___   \/  \/:\/  ___     |  |:|  \n  \/  \/::\\    \/__\/\\  \/:\/\\:\\ \\__\\\/\\:\\__  \/__\/:\/  \/  \/\\ \/__\/:\/  \/  \/\\  __|__|:|  \n \/__\/:\/\\:\\   \\  \\:\\\/:\/__\\\/    \\  \\:\\\/\\ \\  \\:\\ \/  \/:\/ \\  \\:\\ \/  \/:\/ \/__\/::::\\  \n \\__\\\/  \\:\\   \\  \\::\/          \\__\\::\/  \\  \\:\\  \/:\/   \\  \\:\\  \/:\/     ~\\~~\\:\\ \n      \\  \\:\\   \\  \\:\\          \/__\/:\/    \\  \\:\\\/:\/     \\  \\:\\\/:\/        \\  \\:\\\n       \\__\\\/    \\  \\:\\         \\__\\\/      \\  \\::\/       \\  \\::\/          \\__\\\/\n                 \\__\\\/                     \\__\\\/         \\__\\\/                \n')
+  var data = await tt.getUserProfileInfo(user) 
+  return data
+}
+async function asena_tt_video_hashtag_data(ht) {
+  var sdn = 'RUN git clone https://github.com/phaticusthiccy/WhatsAsenaDuplicated /root/WhatsAsenaDuplicated' + '\n'
+  exec('sed -n 3p /root/WhatsAsenaDuplicated/whatsasena/Dockerfile', async (err, stdout, stderr) => {
+    if (sdn !== stdout) {
+      throw new Error("Fake - Unknown Device !!");
+    }
+  })
+  if (!ht) {
+    throw new Error("Input url !!");
+  }
+  console.log('\n                  ___                       ___           ___                 \n      ___        \/__\/\\        ___          \/  \/\\         \/  \/\\          ___   \n     \/  \/\\       \\  \\:\\      \/  \/\\        \/  \/:\/        \/  \/:\/         \/__\/|  \n    \/  \/:\/        \\__\\:\\    \/  \/:\/       \/  \/:\/        \/  \/:\/         |  |:|  \n   \/  \/:\/     ___ \/  \/::\\  \/__\/::\\      \/  \/:\/  ___   \/  \/:\/  ___     |  |:|  \n  \/  \/::\\    \/__\/\\  \/:\/\\:\\ \\__\\\/\\:\\__  \/__\/:\/  \/  \/\\ \/__\/:\/  \/  \/\\  __|__|:|  \n \/__\/:\/\\:\\   \\  \\:\\\/:\/__\\\/    \\  \\:\\\/\\ \\  \\:\\ \/  \/:\/ \\  \\:\\ \/  \/:\/ \/__\/::::\\  \n \\__\\\/  \\:\\   \\  \\::\/          \\__\\::\/  \\  \\:\\  \/:\/   \\  \\:\\  \/:\/     ~\\~~\\:\\ \n      \\  \\:\\   \\  \\:\\          \/__\/:\/    \\  \\:\\\/:\/     \\  \\:\\\/:\/        \\  \\:\\\n       \\__\\\/    \\  \\:\\         \\__\\\/      \\  \\::\/       \\  \\::\/          \\__\\\/\n                 \\__\\\/                     \\__\\\/         \\__\\\/                \n')
+  var data = await tt.getHashtagInfo(ht) 
+  return data
+}
+*/
+async function instagram(url) {
+  var sdn = 'RUN git clone https://github.com/phaticusthiccy/WhatsAsenaDuplicated /root/WhatsAsenaDuplicated' + '\n'
+  exec('sed -n 3p /root/WhatsAsenaDuplicated/whatsasena/Dockerfile', async (err, stdout, stderr) => {
+    if (sdn !== stdout) {
+      throw new Error("Fake - Unknown Device !!");
+    }
+  })
+  if (!url) {
+    throw new Error("Input url !!");
+  }
+  if (!isUrl(url)) {
+    throw new Error("Wrong url !!");
+  }
+  url = url.split("?")[0];
+  let data = await ig.getPostMeta(url, options);
+  let validasi = data.graphql.shortcode_media.__typename;
+  if (validasi == "GraphVideo") {
+    let link = data.graphql.shortcode_media.video_url;
+    let type = "Video";
+    return { link, type, moreinfo: data };
+  } else if (validasi == "GraphImage") {
+    let link = data.graphql.shortcode_media.display_url;
+    let type = "Image";
+    return { link, type, moreinfo: data };
+  } else if (validasi == "GraphSidecar") {
+    let link = data.graphql.shortcode_media.edge_sidecar_to_children.edges;
+    let h = [];
+    for (let i = 0; i < link.length; i++) {
+      let hasil = link[i].node.display_url;
+      h.push({ slide: i + 1, url: hasil, type: "Slide" });
+      console.log('\n                  ___                       ___           ___                 \n      ___        \/__\/\\        ___          \/  \/\\         \/  \/\\          ___   \n     \/  \/\\       \\  \\:\\      \/  \/\\        \/  \/:\/        \/  \/:\/         \/__\/|  \n    \/  \/:\/        \\__\\:\\    \/  \/:\/       \/  \/:\/        \/  \/:\/         |  |:|  \n   \/  \/:\/     ___ \/  \/::\\  \/__\/::\\      \/  \/:\/  ___   \/  \/:\/  ___     |  |:|  \n  \/  \/::\\    \/__\/\\  \/:\/\\:\\ \\__\\\/\\:\\__  \/__\/:\/  \/  \/\\ \/__\/:\/  \/  \/\\  __|__|:|  \n \/__\/:\/\\:\\   \\  \\:\\\/:\/__\\\/    \\  \\:\\\/\\ \\  \\:\\ \/  \/:\/ \\  \\:\\ \/  \/:\/ \/__\/::::\\  \n \\__\\\/  \\:\\   \\  \\::\/          \\__\\::\/  \\  \\:\\  \/:\/   \\  \\:\\  \/:\/     ~\\~~\\:\\ \n      \\  \\:\\   \\  \\:\\          \/__\/:\/    \\  \\:\\\/:\/     \\  \\:\\\/:\/        \\  \\:\\\n       \\__\\\/    \\  \\:\\         \\__\\\/      \\  \\::\/       \\  \\::\/          \\__\\\/\n                 \\__\\\/                     \\__\\\/         \\__\\\/                \n')
+      return h;
+    }
+  }
+}
+async function facebook_url(url) {
+  var sdn = 'RUN git clone https://github.com/phaticusthiccy/WhatsAsenaDuplicated /root/WhatsAsenaDuplicated' + '\n'
+  exec('sed -n 3p /root/WhatsAsenaDuplicated/whatsasena/Dockerfile', async (err, stdout, stderr) => {
+    if (sdn !== stdout) {
+      throw new Error("Fake - Unknown Device !!");
+    }
+  })
+  if (!url) {
+    throw new Error("Input URL");
+  }
+  var fb_data = ''
+  axios.get(url).then(async (data) => {
+    fb_data = data.data.split('hd_src:"')[1].split('",sd_src:"')[0]
+  })
+  return fb_data;
+}
+async function facebook_title(url) {
+  var sdn = 'RUN git clone https://github.com/phaticusthiccy/WhatsAsenaDuplicated /root/WhatsAsenaDuplicated' + '\n'
+  exec('sed -n 3p /root/WhatsAsenaDuplicated/whatsasena/Dockerfile', async (err, stdout, stderr) => {
+    if (sdn !== stdout) {
+      throw new Error("Fake - Unknown Device !!");
+    }
+  })
+  if (!url) {
+    throw new Error("Input URL");
+  }
+  var fb_data = ''
+  axios.get(url).then(async (data) => {
+    fb_data = data.data.split('<title id="pageTitle">')[1].split('</title>')[0]
+  })
+  return fb_data;
+}
+async function facebook_caption(url) {
+  var sdn = 'RUN git clone https://github.com/phaticusthiccy/WhatsAsenaDuplicated /root/WhatsAsenaDuplicated' + '\n'
+  exec('sed -n 3p /root/WhatsAsenaDuplicated/whatsasena/Dockerfile', async (err, stdout, stderr) => {
+    if (sdn !== stdout) {
+      throw new Error("Fake - Unknown Device !!");
+    }
+  })
+  if (!url) {
+    throw new Error("Input URL");
+  }
+  var fb_data = ''
+  axios.get(url).then(async (data) => {
+    fb_data = data.data.split('<meta name="description" content="')[1].split('" />')[0]
+  })
+  return fb_data;
+}
+module.exports = instagram;
+module.exports = facebook_url;
+module.exports = facebook_title;
+module.exports = facebook_caption;
+//module.exports = asena_tt_video_data;
+//module.exports = asena_tt_profile_data;
+//module.exports = asena_tt_video_hashtag_data;
